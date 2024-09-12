e525a822f250f1594f06965d367f54a67f0667bc
ICHINOSE Shogo <shogo82148@gmail.com>
2024-09-12 21:09:00 +0900

    build core libraries as static libraries.

--- build/overwrites/hints/emscripten.sh
+++ build/overwrites/hints/emscripten.sh
@@ -14,6 +14,12 @@ nm="$(which llvm-nm)"  # note from Glossary: 'After Configure runs, the value is
 ar="$(which llvm-ar)"  # note from Glossary: 'After Configure runs, the value is reset to a plain "ar" and is not useful.'
 ranlib="$(which llvm-ranlib)"
 
+
+# dyanmic libraries don't work in emscripten.
+# so we build them as static libraries.
+static_ext="attributes B Cwd Data/Dumper Devel/Peek Digest/MD5 Digest/SHA Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/via re SDBM_File Socket Time/HiRes Time/Piece Unicode/Normalize"
+dynamic_ext=''
+
 man1dir="none"
 man3dir="none"
 
